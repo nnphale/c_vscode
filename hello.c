fa13e6d2e66b37c7d093cc8989e00895ed4e2eb2
bunnie <ngocphale.ng@gmail.com>
2026-04-08 14:54:19 +0700

    Them file hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+// git init -> Khởi tạo git repository ở máy tính
+
+// git remote add origin <URL của remote repository> -> Thêm địa chỉ của kho code trên github
+
+// git status -> Kiểm tra trạng thái local repo
+
+// git add . -> Thêm tất cả các file TRONG FOLDER HIỆN TẠI VÀO STAGING AREA 
+
+// git commit -m "<Mô tả những gì mình làm>" -> "Đẩy" các file từ STAGING AREA vào LOCAL REPO
+
+//   git config --global user.email "you@example.com"
+//   git config --global user.name "Your Name"
+
+int main () {
+    printf("Hello world !")
+}
